afece02a2c71234b09ced2a5362992ba2117ee2f
Natallia Akulich <natallia_akulich@epam.com>
2023-10-17 17:08:20 +0300

    Django app tutorial, part 3

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.http import Http404
+from django.shortcuts import render
+from django.template import loader
+from .models import Question
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world. You are at the poll's index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}s.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}s.")
